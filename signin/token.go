ab559ba27d70818e4d6daff2b21aaea948d1125d
Robert Zhang <robertsky.zhang@gmail.com>
2017-12-17 03:43:48 -0500

    Created token api template

---
+++ signin/token.go
@@ -0,0 +1,12 @@
+package signin
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+//GoogleSignIn handles google token sign in
+func GoogleSignIn(c *gin.Context) {
+	log.Println("handle google signin")
+}
